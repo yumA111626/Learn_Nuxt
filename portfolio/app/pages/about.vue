05b840fc6d6d2f27487a82b4f3e4252e4cd94782
Hanai <yuuukariforev626111@gmail.com>
2026-08-19 12:11:36 +0900

    不要な箇所の削除

--- portfolio/app/pages/about.vue
+++ portfolio/app/pages/about.vue
@@ -2,19 +2,4 @@
     <div>
         This page is about me.
     </div>
-
-    <div>
-        <button @click="enableCustomLayout">
-        Update layout
-        </button>
-    </div>
 </template>
-
-<script setup>
-// definePageMeta({
-//   layout: 'custom',
-// })
-function enableCustomLayout () {
-  setPageLayout('custom')
-}
-</script>
