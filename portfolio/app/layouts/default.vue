f1a17ddf6d07c6dabe51348eb2b6239532f267b0
Hanai <yuuukariforev626111@gmail.com>
2026-08-19 11:55:34 +0900

    layouts に関して記載

---
+++ portfolio/app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <NuxtLink to = "/">Main</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to = "/about">About</NuxtLink>
+            </li>
+        </ul>
+    </nav>
+    <slot />
+</template>
