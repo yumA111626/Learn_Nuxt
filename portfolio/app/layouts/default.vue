f9b77a0de37ad9141641819f6d6caf8b1dc6724a
Hanai <yuuukariforev626111@gmail.com>
2026-08-21 17:51:04 +0900

    components を追加

--- portfolio/app/layouts/default.vue
+++ portfolio/app/layouts/default.vue
@@ -1,19 +1,10 @@
 <template>
     <div class="container mx-auto max-w-2xl">
-        <header class="flex justify-between">
+        <header class="flex justify-between items-center mt-5">
             <div>
-                <NuxtLink to = "/" class="text-xl font-semibold p-2 hover:bg-gray-200  ">Yuma Hanai</NuxtLink>
+                <NuxtLink to = "/" class="text-xl font-semibold p-2 hover:bg-gray-200">Yuma Hanai</NuxtLink>
             </div>
-            <nav>
-                <ul class="flex space-x-4">
-                    <li>
-                        <NuxtLink to = "/">Main</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to  = "/about">About</NuxtLink>
-                    </li>   
-                </ul>
-            </nav>
+            <Menu />
         </header> 
         <main class="p-2">
             <slot />
