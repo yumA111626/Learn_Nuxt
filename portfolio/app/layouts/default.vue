4937fe97b56841cf978edc7b127cd36238622d27
Hanai <yuuukariforev626111@gmail.com>
2026-08-21 17:32:04 +0900

    header mainを追加

--- portfolio/app/layouts/default.vue
+++ portfolio/app/layouts/default.vue
@@ -1,13 +1,22 @@
 <template>
-    <nav>
-        <ul>
-            <li>
-                <NuxtLink to = "/">Main</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to = "/about">About</NuxtLink>
-            </li>
-        </ul>
-    </nav>
-    <slot />
+    <div class="container mx-auto max-w-2xl">
+        <header class="flex justify-between">
+            <div>
+                <NuxtLink to = "/" class="text-xl font-semibold p-2 hover:bg-gray-200  ">Yuma Hanai</NuxtLink>
+            </div>
+            <nav>
+                <ul class="flex space-x-4">
+                    <li>
+                        <NuxtLink to = "/">Main</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to  = "/about">About</NuxtLink>
+                    </li>   
+                </ul>
+            </nav>
+        </header> 
+        <main class="p-2">
+            <slot />
+        </main> 
+    </div>
 </template>
