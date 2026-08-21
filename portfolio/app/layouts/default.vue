2aa649cc70f980a96a30b0ab99072a9ab08aab28
Hanai <yuuukariforev626111@gmail.com>
2026-08-21 18:21:56 +0900

    google font を追加

--- portfolio/app/layouts/default.vue
+++ portfolio/app/layouts/default.vue
@@ -11,3 +11,26 @@
         </main> 
     </div>
 </template>
+
+<script setup>
+useHead({
+    titleTemplate : '%s - Yuma Hanai',
+    link: [
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com',
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+            crossorigin: '',
+        },
+    ],
+})
+</script>
+
+<style>
+body {
+    font-family: "Roboto";
+}
+</style>
--- portfolio/app/pages/index.vue
+++ portfolio/app/pages/index.vue
@@ -3,3 +3,31 @@
         Hello welcome page
     </div>
 </template>
+
+<script setup>
+useSeoMeta({
+    title: 'Main page',
+    description: 'This is Main Page',
+    ogTitle: '[og:title]',
+    ogDescription: '[og:description]',
+    ogImage: '[og:image]',
+    ogUrl: '[og:url]',
+    twitterTitle: '[twitter:title]',
+    twitterDescription: '[twitter:description]',
+    twitterImage: '[twitter:image]',
+    twitterCard: 'summary'
+})
+
+useHead({
+    htmlAttrs: {
+    lang: 'en'
+    },
+    link: [
+    {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon.png'
+    }
+    ]
+})
+</script>
